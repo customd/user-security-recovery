c824ac262b06138f748bc517b33a32228f39548b
Craig Smith <952595+phpsa@users.noreply.github.com>
2021-10-21 11:42:53 +1300

    fix: should throw exception if password not valid
    * Update EncryptedRecovery.php
    
    * Update HasRecovery.php
    
    * Update HasRecovery.php

--- src/Contracts/HasRecovery.php
+++ src/Contracts/HasRecovery.php
@@ -46,7 +46,9 @@ trait HasRecovery
         if ($this->recoveryRecord === null) {
             throw new SecurityNotFoundException('No Recovery record found');
         }
-        return $this->recoveryRecord->validateAnswer($this->getRecoveryAnswer());
+        
+        throw_unless($this->recoveryRecord->validateAnswer($this->getRecoveryAnswer()), new SecurityNotFoundException('Password not valid')); 
+
     }
 
         /**
